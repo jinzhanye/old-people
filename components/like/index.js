9e75475366ca7ed2b5f382525bd3b309f2de178b
jinzhanye <jinzhan.ye@foxmail.com>
2019-04-16 13:40:27 +0800

    completed like component

---
+++ components/like/index.js
@@ -0,0 +1,50 @@
+// components/like/index.js
+const LIKE_IMG_PATH = 'images/like.png';
+const DIS_LIKE_IMG_PATH = 'images/like@dis.png';
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    count: {
+      type: Number,
+      value: 8,
+    },
+    like: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    img: DIS_LIKE_IMG_PATH,
+  },
+
+  observers: {
+    like(isLike) {
+      this.setData({
+        img: isLike ? LIKE_IMG_PATH : DIS_LIKE_IMG_PATH,
+      });
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    toggle() {
+      const { like, count } = this.properties;
+      this.setData({
+        count: like ? count - 1 : count + 1,
+        like: !like,
+      });
+      this.triggerEvent('like', {
+        behavior: this.properties.like ? 'like' : 'cancel',
+      });
+    }
+  }
+});
